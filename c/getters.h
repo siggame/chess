3ea39ca8e7b3d048052a34407f7b66fb8500eefc
Goldman <brian.goldman@mst.edu>
2012-02-08 12:12:47 -0600

    Removed some DLLEXPORT commands that didn't appear in old version

--- c/getters.h
+++ c/getters.h
@@ -2,35 +2,29 @@
 #define GETTERS_H
 #include "structures.h"
 
-#ifdef _WIN32
-#define DLLEXPORT extern "C" __declspec(dllexport)
-#else
-#define DLLEXPORT
-#endif
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-DLLEXPORT int moveGetId(_Move* ptr);
-DLLEXPORT int moveGetFromFile(_Move* ptr);
-DLLEXPORT int moveGetFromRank(_Move* ptr);
-DLLEXPORT int moveGetToFile(_Move* ptr);
-DLLEXPORT int moveGetToRank(_Move* ptr);
-DLLEXPORT int moveGetPromoteType(_Move* ptr);
+int moveGetId(_Move* ptr);
+int moveGetFromFile(_Move* ptr);
+int moveGetFromRank(_Move* ptr);
+int moveGetToFile(_Move* ptr);
+int moveGetToRank(_Move* ptr);
+int moveGetPromoteType(_Move* ptr);
 
 
-DLLEXPORT int pieceGetId(_Piece* ptr);
-DLLEXPORT int pieceGetOwner(_Piece* ptr);
-DLLEXPORT int pieceGetFile(_Piece* ptr);
-DLLEXPORT int pieceGetRank(_Piece* ptr);
-DLLEXPORT int pieceGetHasMoved(_Piece* ptr);
-DLLEXPORT int pieceGetType(_Piece* ptr);
+int pieceGetId(_Piece* ptr);
+int pieceGetOwner(_Piece* ptr);
+int pieceGetFile(_Piece* ptr);
+int pieceGetRank(_Piece* ptr);
+int pieceGetHasMoved(_Piece* ptr);
+int pieceGetType(_Piece* ptr);
 
 
-DLLEXPORT int playerGetId(_Player* ptr);
-DLLEXPORT char* playerGetPlayerName(_Player* ptr);
-DLLEXPORT float playerGetTime(_Player* ptr);
+int playerGetId(_Player* ptr);
+char* playerGetPlayerName(_Player* ptr);
+float playerGetTime(_Player* ptr);
 
 
 
