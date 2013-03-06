76ad16947b6da4aa4eddfe296c8e03880aea33ff
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2013-03-06 08:45:59 -0600

    Added DLLEXPORT to functions in getters
    While Mono appears to work fine without this construct, testing with
    Visual Studio 2010 running on Windows 8 throws a
    EntryPointNotFoundException. Closes #3.

--- c/getters.h
+++ c/getters.h
@@ -1,30 +1,31 @@
 #ifndef GETTERS_H 
 #define GETTERS_H
 #include "structures.h"
+#include "game.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int moveGetId(_Move* ptr);
-int moveGetFromFile(_Move* ptr);
-int moveGetFromRank(_Move* ptr);
-int moveGetToFile(_Move* ptr);
-int moveGetToRank(_Move* ptr);
-int moveGetPromoteType(_Move* ptr);
+DLLEXPORT int moveGetId(_Move* ptr);
+DLLEXPORT int moveGetFromFile(_Move* ptr);
+DLLEXPORT int moveGetFromRank(_Move* ptr);
+DLLEXPORT int moveGetToFile(_Move* ptr);
+DLLEXPORT int moveGetToRank(_Move* ptr);
+DLLEXPORT int moveGetPromoteType(_Move* ptr);
 
 
-int pieceGetId(_Piece* ptr);
-int pieceGetOwner(_Piece* ptr);
-int pieceGetFile(_Piece* ptr);
-int pieceGetRank(_Piece* ptr);
-int pieceGetHasMoved(_Piece* ptr);
-int pieceGetType(_Piece* ptr);
+DLLEXPORT int pieceGetId(_Piece* ptr);
+DLLEXPORT int pieceGetOwner(_Piece* ptr);
+DLLEXPORT int pieceGetFile(_Piece* ptr);
+DLLEXPORT int pieceGetRank(_Piece* ptr);
+DLLEXPORT int pieceGetHasMoved(_Piece* ptr);
+DLLEXPORT int pieceGetType(_Piece* ptr);
 
 
-int playerGetId(_Player* ptr);
-char* playerGetPlayerName(_Player* ptr);
-float playerGetTime(_Player* ptr);
+DLLEXPORT int playerGetId(_Player* ptr);
+DLLEXPORT char* playerGetPlayerName(_Player* ptr);
+DLLEXPORT float playerGetTime(_Player* ptr);
 
 
 
